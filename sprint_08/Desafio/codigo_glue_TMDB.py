0c1d5b5b202c1e2b856bfc88615fccd262cb7a9c
Carolina Cartaxo <110704343+carolinacartaxo@users.noreply.github.com>
2024-08-19 19:52:09 -0300

    Update codigo_glue_TMDB.py

--- sprint_08/Desafio/codigo_glue_TMDB.py
+++ sprint_08/Desafio/codigo_glue_TMDB.py
@@ -16,15 +16,12 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
-# Caminho de entrada e saída no S3
 source_file = args['S3_INPUT_PATH']
 target_path = args['S3_TARGET_PATH']
 
-# Extrair a data de coleta do caminho S3
 s3_path_parts = source_file.split('/')
 data_coleta = f"{s3_path_parts[-4]}-{s3_path_parts[-3]}-{s3_path_parts[-2]}"
 
-# Leitura dos dados JSON do TMDB da Raw Zone
 df = glueContext.create_dynamic_frame.from_options(
     "s3",
     {
@@ -34,15 +31,12 @@ df = glueContext.create_dynamic_frame.from_options(
     {"multiline": False}
 )
 
-# Verificação do esquema inicial
 print("Esquema inicial:")
 df.printSchema()
 
-# Adicionando a coluna 'dt' com a data de coleta formatada como 'yyyy-MM-dd'
 spark_df = df.toDF()
 spark_df = spark_df.withColumn("dt", lit(data_coleta))
 
-# Verificação do esquema e amostra dos dados
 print("Esquema após adicionar a coluna de data:")
 spark_df.printSchema()
 spark_df.show(5)
@@ -50,7 +44,6 @@ spark_df.show(5)
 # Convertendo de volta para DynamicFrame
 dynamic_frame = DynamicFrame.fromDF(spark_df, glueContext, "dynamic_frame")
 
-# Escrita dos dados particionados no S3 em formato Parquet
 glueContext.write_dynamic_frame.from_options(
     frame=dynamic_frame,
     connection_type="s3",
